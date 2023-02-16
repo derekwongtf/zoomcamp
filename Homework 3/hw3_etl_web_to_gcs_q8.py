750f4bf9ec5b09ae0003b589bac77ee2bcd4c5f4
derekwongtf <derekwongtf@gmail.com>
2023-02-16 22:44:58 +0800

    Update Homework3

---
+++ Homework 3/hw3_etl_web_to_gcs_q8.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+# read_csv in chunk is for computers where limited memory is available for reading large csv file in once. 
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    # if randint(0, 1) > 0:
+    #     raise Exception
+    df = pd.DataFrame()
+    with pd.read_csv(dataset_url, chunksize=3000000) as reader:
+        for chunk in reader:
+            print(len(chunk))
+            df = pd.concat([df, chunk])
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
+    """Fix dtype issues"""
+    if color == "yellow":
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    elif color == "green":
+        df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    else:
+        df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+        df["dropOff_datetime"] = pd.to_datetime(df["dropOff_datetime"])
+        # See https://pandas.pydata.org/docs/user_guide/integer_na.html
+        df["PUlocationID"] = df["PUlocationID"].astype('Int64')  
+        df["DOlocationID"] = df["DOlocationID"].astype('Int64')
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    Path(f"data/{color}").mkdir(parents=True, exist_ok=True)
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block.upload_from_path(from_path=path, to_path=path, timeout=120)
+    return
+
+
+@flow()
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+    """The main ETL function"""
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df,color)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(
+    months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"
+):
+    for month in months:
+        etl_web_to_gcs(year, month, color)
+
+
+if __name__ == "__main__":
+    color = "fhv"
+    months = list(range(1,13))
+    year = 2019
+    etl_parent_flow(months, year, color)
